4041f0ce153936376904d72f9a7ca2f9e1f7a6a4
Joushy <johannakristiinasaar@gmail.com>
2020-09-13 23:25:17 +0300

    frontti nodeen

---
+++ aapl/build/precache-manifest.6f75171cb136c765bfb69d1ab0351444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fdb02fe8c0368b3e4423e7d29edb0db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e5c527601905f041cf4",
+    "url": "/static/css/main.45022bf5.chunk.css"
+  },
+  {
+    "revision": "7e66bff5c5e35e2bc7d7",
+    "url": "/static/js/2.d726b825.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d726b825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e5c527601905f041cf4",
+    "url": "/static/js/main.217b8fa0.chunk.js"
+  },
+  {
+    "revision": "378ded2a330d4d35338d",
+    "url": "/static/js/runtime-main.aab24361.js"
+  }
+]);
